4f2caddbf2c6d132dc9b4ce56da1f4a600be4a06
Ljqiii <ljq917181927@gmail.com>
2019-07-15 02:18:35 -0700

    修改刷新逻辑

--- src/main/resources/static/js/admin/main.js
+++ src/main/resources/static/js/admin/main.js
@@ -1,22 +1,32 @@
 $(function () {
+
+
     $(".blog-menu .list-group-item").click(function () {
+
         var url = $(this).attr("url");
+
+        $("#titleid").data("rightContainerurl", url)
 
         $(".blog-menu .list-group-item").removeClass("active");
         $(this).addClass("active");
 
-        $.ajax({
-            url:url,
-            success:function (data) {
-                $("#rightContainer").html(data);
-            },
-            error:function () {
-                alert("error");
-            }
-        });
+        showright(url)
+
     });
 
 
     $(".blog-menu .list-group-item:first").trigger("click");
 
 });
+
+function showright(url) {
+    $.ajax({
+        url: url,
+        success: function (data) {
+            $("#rightContainer").html(data);
+        },
+        error: function () {
+            alert("error");
+        }
+    });
+}
